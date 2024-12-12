3bde904985673dd8a482751486dbe01c0e56903a
AlTap204 <133538933+AlTap204@users.noreply.github.com>
2024-12-12 13:08:54 -0700

    Delete api python directory

--- api python/apicaller.py
+++
@@ -1,8 +0,0 @@
-import requests
-numero1 = float(input("Ingresa numero 1: "))
-numero2 = float(input("Ingresa numero 2: "))
-url = f"http://localhost/apis/suma.php?numero1={numero1}&numero2={numero2}"
-response = requests.post(url)
-responsedata = response.json()
-print (responsedata)
-#print (f"El resultado es: {responsedata['result']}")
